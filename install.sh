30289683972845503a09974fac1e1b05b6508f95
alagroy- <alagroy-@student.42.fr>
2021-05-12 14:52:51 +0200

    setting default shell to zsh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#!/bin/zsh
 
 DIR="$HOME/scripts"
 mkdir -p $DIR
+SHELL="/bin/zsh"
 
 command -v git >/dev/null 2>/dev/null
 
@@ -11,9 +12,9 @@ fi
 
 mkdir -p $DIR
 
-git clone https://github.com/alagroy-42/ft_select.git $DIR/ft_select
+git clone https://github.com/alagroy-42/ft_select.git $DIR/ft_select >/dev/null 2>&1 
 
-make -C ~/scripts/ft_select
+make -C ~/scripts/ft_select >/dev/null 2>&1 
 
 cp push.sh $DIR/push.sh	
 
@@ -25,3 +26,6 @@ fi
 
 SHELL_FILE=~/.`echo $SHELL | rev | cut -d'/' -f1 | rev`rc
 echo "export PATH=\$PATH:"$DIR >> $SHELL_FILE
+echo "alias push=push.sh" >> $SHELL_FILE
+
+echo 'push_script successfully installed'
